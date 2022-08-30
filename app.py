f1d306f8051924a3e316cbf0811b4f1bbf5d07de
HitalloAzevedo <azevedohitallo@gmail.com>
2022-08-30 19:50:25 -0300

    new files

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 
 
 app = Flask(__name__)
@@ -7,21 +7,24 @@ app = Flask(__name__)
 languages = [
     {'name': 'Javascript'},
     {'name': 'C#'},
-    {'name': 'Java'}
+    {'name': 'Java'},
+    {'name': 'PHP'},
+    {'name': 'Dart'},
+    {'name': 'C++'}
 ]
 
 
 @app.get('/')
 def main():
-    return "<h1>A API ESTÁ NO AR!</h1> <p>Acesse /langs para visualizar</p> <footer>Desenvolvida por Hitallo Azevedo</footer>"
+    return render_template('index.html')
 
 
-@app.get('/langs')
+@app.get('/api/langs')
 def returnAll():
     return jsonify({'languages': languages})
 
 
-@app.post('/langs')
+@app.post('/api/langs')
 def addLang():
     language = request.get_json('name')
 
